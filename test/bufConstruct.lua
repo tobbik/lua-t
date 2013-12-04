d47c967c00b87129b47c62dcfc1d2fe8a5efcf0f
Tobias Kieslich <tobias.kieslich@gmail.com>
2013-12-04 14:13:18 -0800

    Define the way Tests will be written better

--- test/bufConstruct.lua
+++ test/bufConstruct.lua
@@ -3,15 +3,15 @@
 ---
 -- \file    bufConstruct.c
 -- \brief   basic tests of xt.Buffer Constructor
-local t = require("xt").Test()
+local t = require("xt").Test('Test basic xt.Buffer functionality')
 
-t:setUp = function(self)
+t:setUp = function("Initiate 2 Buffers, one from string one filled manually")
 	-- assemble the test strings
 	self.n       = 255
 	local sH,sB  = {},{} --string Hex and string Binary
 	for i=0,self.n do
-		sT[i+1] = string.format('%02X ', i)
-		sT[i+1] = string.char(i)
+		sH[i+1] = string.format('%02X ', i)
+		sB[i+1] = string.char(i)
 	end
 	self.strBin = table.concat (sB, '')
 	self.strHex = table.concat (sH, '')
@@ -24,18 +24,24 @@ t:setUp = function(self)
 	self.bufStr  = xt.Buffer(self.strBin)
 end
 
-t:testSameContent = function(self)
-	self:equal (self.buf8:toHex(), self.bufS:toHex())
-	self:equal (self.buf8:toHex(), self.sH)
+t:test1_SameContent = function('Test the equality of the buffers')
+	self:_equal (self.buf8:toHex(), self.bufS:toHex())
+	self:_equal (self.buf8:toHex(), self.sH)
+	print ('buffer content Hexadecimal representation is not was expected')
+end
+
+t:test2_SameLength = function('Buffers have the same length and are equal to string length')
+	self:_equal (#self.buf8, self.buf8.length())
+	self:_equal (#self.buf8, #self.bufS)
+	self:_equal (#self:buf8, self.n)
 	print ('buffer conten Hexadecimal representtion is not was expected')
 end
 
-t:testSameceLength = function(self)
-	self:equal (#self.buf8, self.buf8.length())
-	self:equal (#self.buf8, #self.bufS)
-	self:equal (#self:buf8, self.n)
-	print ('buffer conten Hexadecimal representtion is not was expected')
+t:tearDown = function("TearDown")
+	self.n       = nil
+	self.strBin  = nil
+	self.strHex  = nil
+	-- create buffer of 255 bytes len and fill with numeric byte sized values
+	self.buf8    = nil
+	self.bufStr  = nil
 end
-
-
-t:run()
