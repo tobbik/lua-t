21ba50e29d2e63688cb8ffdd36a6055b754df930
Tobias Kieslich <tobias.kieslich@gmail.com>
2014-10-30 10:29:19 -0700

    Improve xt.Loop example script

--- example/loop.lua
+++ example/loop.lua
@@ -4,23 +4,20 @@ l = nil
 
 p = function( ... )
 	print( table.unpack( {...} ) )
-	return xt.Time( 5000 )
+	return xt.Time( math.random( 1500,4600 ) )
 end
 
-f = function(func, ...)
-	local args = {...}
-	return function()
-		return func( table.unpack( args ) )
-	end
-end
 
 function r(s)
 	local msg, len, ip_cli = s:recvFrom()
 	print(msg, len, ip_cli, "\n")
-	if msg:sub( 1, 4 ) == 'exit' then print( "go exit" ) ;l:stop() end
+	if msg:sub( 1, 4 ) == 'exit' then
+		print( "go exit" )
+		l:stop()
+	end
 end
 
-l   = xt.Loop( 40 )
+l   = xt.Loop( 40 )      ---< Loop with 40 slots for filehandles
 tm1 = xt.Time( 1500 )
 tm2 = xt.Time( 2700 )
 tm3 = xt.Time( 3300 )
@@ -31,12 +28,12 @@ tm7 = xt.Time( 7400 )
 -- s  = xt.Socket.bind( 'UDP', '192.168.0.219', 8888 )
 -- l:addHandle( s, true, r, s )
 print('' , tm1, '\n', tm2, '\n', tm3, '\n', tm4, '\n', tm5, '\n', tm6, '\n', tm7)
-l:addTimer( tm4, f(print, "-----------------Timer 4------------------------") )
-l:addTimer( tm6, print, "-----------------Timer 6------------------------")
-l:addTimer( tm7, print, "-----------------Timer 7------------------------")
-l:addTimer( tm1, f(print, "-----------------Timer 1------------------------") )
-l:addTimer( tm3, print, "-----------------Timer 3------------------------")
+l:addTimer( tm4, p, "-----------------Timer 4------------------------" )
+l:addTimer( tm6, p, "-----------------Timer 6------------------------" )
+l:addTimer( tm7, p, "-----------------Timer 7------------------------" )
+l:addTimer( tm1, p, "-----------------Timer 1------------------------" )
+l:addTimer( tm3, p, "-----------------Timer 3------------------------" )
 l:addTimer( tm2, p, "-----------------Timer 2------------------------" )
-l:addTimer( tm5, print, "-----------------Timer 5------------------------")
+l:addTimer( tm5, p, "-----------------Timer 5------------------------" )
 
 l:run()
