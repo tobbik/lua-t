5a698e878cd69291279152023974095f64f89646
Tobias Kieslich <tobias.kieslich@gmail.com>
2014-12-02 15:13:10 -0800

    Convert Struct example to new style

--- example/struct.lua
+++ example/struct.lua
@@ -2,55 +2,56 @@
 t=require't'
 
 
-p = t.Pack.IntB2
-c = t.Pack.Int2
+p = t.Pack('<i2')
+c = t.Pack('>i2')
 
-ts = t.Pack.Sequence (
+ts = t.Pack(
 	p,
-	t.Pack.Int2,
-	t.Pack.Int1,
-	t.Pack.Int1
+	t.Pack('>i2'),
+	t.Pack('i1'),
+	t.Pack('i1')
 )
 
-ss = t.Pack.Sequence (
+ss = t.Pack (
 	p,
 	p,
-	t.Pack.Sequence (
-		t.Pack.Bit1,  -- is the train set healthy?
-		t.Pack.Bit2,  -- is the train set stationary?
-		t.Pack.Bit3,  -- is the train set in maint mode?
-		t.Pack.Bit4,  -- is a passenger request active?
-		t.Pack.Bit5,  -- is a file waiting for download (RTDM ready)
-		t.Pack.Bit6,  -- is the connection to the vmds lost?
-		t.Pack.Bit7,  -- is the train set in shop mode?
-		t.Pack.Bit8   -- padding
-	),
-	t.Pack.Int1,
-	t.Pack.Int1
+	'rrrrrrrr',
+	--t.Pack.Sequence (
+	--	t.Pack.Bit1,  -- is the train set healthy?
+	--	t.Pack.Bit2,  -- is the train set stationary?
+	--	t.Pack.Bit3,  -- is the train set in maint mode?
+	--	t.Pack.Bit4,  -- is a passenger request active?
+	--	t.Pack.Bit5,  -- is a file waiting for download (RTDM ready)
+	--	t.Pack.Bit6,  -- is the connection to the vmds lost?
+	--	t.Pack.Bit7,  -- is the train set in shop mode?
+	--	t.Pack.Bit8   -- padding
+	--),
+	t.Pack('i1'),
+	t.Pack('i1')
 )
 
 
-s = t.Pack.Struct (
-	{ length       = t.Pack.Int2 },
-	{ ['type']     = t.Pack.Int2 },
-	{ ['@status']  = t.Pack.Int1 },
-	{ ConsistCount = t.Pack.Int1 }
+s = t.Pack (
+	{ length       = t.Pack('<i2') },
+	{ ['type']     = t.Pack('<i2') },
+	{ ['@status']  = t.Pack('i1') },
+	{ ConsistCount = t.Pack('i1') }
 )
 sN= t.Pack('i2i2i1i1')
 
-sbits   = t.Pack.Struct (
-	{isHealthy   = t.Pack.Bit1},  -- is the train set healthy?
-	{isZeroSpeed = t.Pack.Bit2},  -- is the train set stationary?
-	{isMaintMode = t.Pack.Bit3},  -- is the train set in maint mode?
-	{isPassReq   = t.Pack.Bit4},  -- is a passenger request active?
-	{isFileForDl = t.Pack.Bit5},  -- is a file waiting for download (RTDM ready)
-	{isVmdsConnd = t.Pack.Bit6},  -- is the connection to the vmds lost?
-	{isShopMode  = t.Pack.Bit7},  -- is the train set in shop mode?
-	{paddingBit  = t.Pack.Bit8}   -- padding
+sbits   = t.Pack (
+	{isHealthy   = 'r'},  -- is the train set healthy?
+	{isZeroSpeed = 'r'},  -- is the train set stationary?
+	{isMaintMode = 'r'},  -- is the train set in maint mode?
+	{isPassReq   = 'r'},  -- is a passenger request active?
+	{isFileForDl = 'r'},  -- is a file waiting for download (RTDM ready)
+	{isVmdsConnd = 'r'},  -- is the connection to the vmds lost?
+	{isShopMode  = 'r'},  -- is the train set in shop mode?
+	{paddingBit  = 'r'}   -- padding
 )
 
 
-tr = t.Pack.Struct (
+tr = t.Pack (
 	{ length    = p },
 	{ ['type']  = p },
 -- 	-- BitField is a special type as the constructor resets the actual offset for each single bit
@@ -70,7 +71,3 @@ b=t.Buffer( 'ABCDEFGHIJKLMNOPQRST' )
 for k,v in pairs(tr) do print( k, v, v(b) ) end
 for k,v in pairs(tr.status) do print( k, v, v(b) ) end
 
-
-
-
-a = t.Pack.Array( t.Pack.IntB2, 4 )
