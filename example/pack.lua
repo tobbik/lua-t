7b614704e1bf49163f496fd01c1b853233011f21
Tobias Kieslich <tobias.kieslich@gmail.com>
2014-12-02 17:22:47 -0800

    Create comprehensive example of Packer Sequence/Struct use
     - replace old pack.lua

--- example/pack.lua
+++ example/pack.lua
@@ -1,40 +1,26 @@
-#!../out/bin/lua
-t = require("t")
-fmt= string.format
+T = require't'
 
-b1 = t.Pack.Bit1
-b2 = t.Pack.Bit2
-b3 = t.Pack.Bit3
-b4 = t.Pack.Bit4
-b5 = t.Pack.Bit5
-b6 = t.Pack.Bit6
-b7 = t.Pack.Bit7
-b8 = t.Pack.Bit8
-p  = t.Pack.IntB2
-bt = t.Pack.Byte
-nl = t.Pack.NibbleL
-nh = t.Pack.NibbleH
+p   = { 24898, 17507, 101, 70 }
+fmt = '>i2<i2bb'
+s   = string.pack( fmt, table.unpack( p ) )
 
-print( p, nl, nh )
+sq = T.Pack( fmt )          -- autodetect this as multiple packers -> create sequence
 
-print( p:pack( 17519 ), t.Buffer( p:pack( 17519 ) ):toHex() )
+st = T.Pack(                -- key/value pairs create Struct 
+	{ length       = sq[1] },  -- reuse definitions from sq ..
+	{ ['type']     = sq[2] },
+	{ ['@status']  = sq[3] },
+	{ Count        = sq[4] }
+)
 
-print( p:unpack( 'Do' ) )
+print( s, string.unpack( fmt, s ) )
+print( s, table.unpack( sq(s) ) )
 
-print( string.format("%02X", nl:pack( 0xA ):byte(1) ) )
-print( string.format("%02X", nh:pack( 0x6 ):byte(1) ) )
+print()
+-- the result of pair(st) is using the internal ordering mechanism!
+for k,v in pairs( st ) do print( k, v, v(s) ) end
 
-print( nl:unpack( string.char(171) ) )     -- 0xAB
-print( nh:unpack( string.char(171) ) )
-
-
-print( b1, fmt("%02X", b1:pack( true ):byte(1) ) )
-print( b2, fmt("%02X", b2:pack( true ):byte(1) ) )
-print( b3, fmt("%02X", b3:pack( true ):byte(1) ) )
-print( b4, fmt("%02X", b4:pack( true ):byte(1) ) )
-print( b5, fmt("%02X", b5:pack( true ):byte(1) ) )
-print( b6, fmt("%02X", b6:pack( true ):byte(1) ) )
-print( b7, fmt("%02X", b7:pack( true ):byte(1) ) )
-print( b8, fmt("%02X", b8:pack( true ):byte(1) ) )
-
+print()
+-- the result of st(s) is a table -> iteration not ordered!
+for k,v in pairs( st(s) ) do print( k,v ) end
 
