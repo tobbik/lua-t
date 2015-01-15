08c9de3aa17a18b8ea9905e5e46635222614fa86
Tobias Kieslich <tobias.kieslich@gmail.com>
2015-01-15 13:31:06 -0800

    Add a proper file header to t_tim.h

--- t/t_tim.h
+++ t/t_tim.h
@@ -1,3 +1,26 @@
+/* vim: ts=3 sw=3 sts=3 tw=80 sta noet list
+*/
+/**
+ * \file      t_tim.h
+ * \brief     data types for timeval (T.Time) related data types
+ * \author    tkieslich
+ * \copyright See Copyright notice at the end of t.h
+ */
+
+
+//  _____                      
+// |_   _|   _ _ __   ___  ___ 
+//   | || | | | '_ \ / _ \/ __|
+//   | || |_| | |_) |  __/\__ \
+//   |_| \__, | .__/ \___||___/
+// 		|___/|_|              
+
+// there is NO "struct t_tim" type because it is perfectly fine to reuse
+// struct timeval from time.h
+
+
+
+// Constructors
 // t_tim.c
 struct timeval *t_tim_check_ud ( lua_State *luaVM, int pos, int check );
 struct timeval *t_tim_create_ud( lua_State *luaVM );
@@ -9,11 +32,12 @@ void     t_tim_sub  ( struct timeval *tA, struct timeval *tB, struct timeval *tX
 void     t_tim_since( struct timeval *tA );
 long     t_tim_getms( struct timeval *tA );
 
+
 /**--------------------------------------------------------------------------
  * Compare timeval a to timeval b.
  * \param  *a  struct timeval pointer
  * \param  *b  struct timeval pointer
- * \param  CMP comparator
+ * \param  CMP comparator  (any of ==, <, >)
  * \return 1 if a CMP b is true
  * e       0 if a CMP b is false
  * --------------------------------------------------------------------------*/
