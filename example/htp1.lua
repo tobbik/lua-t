27f541ac02a92dfd294b4caa9dafd34ab34f3968
Tobias Kieslich <tobias.kieslich@gmail.com>
2015-02-03 10:13:44 -0800

    Rewire the test scripts to do more meaningful performance testing

--- example/htp1.lua
+++ example/htp1.lua
@@ -4,19 +4,32 @@ fmt=string.format
 l=t.Loop(10)
 l1="This is the first part before we finish. A string te repeated soo man times\n"
 l2="This is the finish line of the response.\n"
-rep=3000
-rx = string.rep( l1, rep )
+rp  = 300
+rc  = 10
+s00 = string.rep( l1, rp )
 
-x=function( msg )
-	--msg:writeHead( 200, #l2 + (rep*#rx) )
-	--msg:write( l1 )
-	msg:finish( rx .. l2 )
-	--l:show()
+x00=function( msg )
+	msg:writeHead( 200, (rc * #s00) + #l2 )
+	for i=1,rc do
+		msg:write( s00 )
+	end
+	msg:finish( l2 )
 end
 
-h=t.Http.Server( l, x )
-sc,ip = h:listen( 8000, 10 )  -- listen on 0.0.0.0 INADDR_ANY
-print( sc, ip )
+
+s01  = string.rep( l1, rp*rc )
+
+x01=function( msg )
+	--msg:writeHead( 200, #l2 + (rep*#rx) )
+	msg:finish( s01 .. l2 )
+end
+
+h00=t.Http.Server( l, x00 )
+h01=t.Http.Server( l, x01 )
+sc00,ip00 = h00:listen( 8000, 10 )  -- listen on 0.0.0.0 INADDR_ANY
+sc01,ip01 = h01:listen( 8001, 10 )  -- listen on 0.0.0.0 INADDR_ANY
+print( sc00, ip00 )
+print( sc01, ip01 )
 
 l:show()
 l:run()
--- example/srv_w.js
+++ example/srv_w.js
@@ -1,16 +1,35 @@
 var http = require('http');
+function repeat(s, n)
+{
+	var a = [];
+	while(a.length < n)
+	{
+		a.push(s);
+	}
+	return a.join('');
+}
 
-var l1 = "This is the first part"
-var l2 = "Be done with this Http Request"
+var l1="This is the first part before we finish. A string te repeated soo man times\n"
+var l2="This is the finish line of the response.\n"
 
-var server = http.createServer(function(req, res) {
-	res.writeHead( 200 , {'Content-Length': 5*l1.length + l2.length} );
-	res.write( l1 );
-	res.write( l1 );
-	res.write( l1 );
-	res.write( l1 );
-	res.write( l1 );
+var rp = 300
+var rc = 10
+var s00 = repeat( l1, rp )
+
+var h00= http.createServer( function(req, res) {
+	// res.writeHead( 200 , {'Content-Length': rc*s00.length + l2.length} );
+	for ( var i=0; i<rc; i++)
+	{
+		res.write( s00 );
+	}
 	res.end( l2 );
 });
 
-server.listen(8002);
+var s01 = repeat( l1, rp*rc )
+var h01= http.createServer( function(req, res) {
+	// res.writeHead( 200 , {'Content-Length': s01.length + l2.length} );
+	res.end( s01 + l2 );
+});
+
+h00.listen(9000);
+h01.listen(9001);
