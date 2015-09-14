d2ed423ef5a91bb9ab561b2a903a52e8cd9e1f4d
Tobias Kieslich <tobias.kieslich@gmail.com>
2015-09-13 23:14:11 -0700

    t_net: Adjust examples to make new networking code work as expected
     - fix glitches within T.Net libraries

--- example/dl_http.lua
+++ example/dl_http.lua
@@ -1,13 +1,13 @@
 #!../out/bin/lua
 local t=require('t')
 
-local tcpsock,ip = t.Socket.connect('TCP','128.30.52.37', 80)
-local len     = tcpsock:send("GET /TR/REC-html32.html HTTP/1.0\r\n\r\n")
+local tcpsock,ip = t.Net.TCP.connect( '128.30.52.37', 80 )
+local len     = tcpsock:send( "GET /TR/REC-html32.html HTTP/1.0\r\n\r\n" )
 local buffer = {}
 local length = 0
 while len>0 do
-	msg, len = tcpsock:recv()
-	table.insert(buffer, msg)
+	msg, len = tcpsock:recv( )
+	table.insert( buffer, msg )
 	length = length + len
 	print(len)
 end
